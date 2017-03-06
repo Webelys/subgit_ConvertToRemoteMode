26a34efacec7d973b6b4d369fd911c4b4f24514e
cam.lafit <cam.lafit@azerttyu.net>
2017-03-06 11:26:04 +0100

    Loop on all git repositories

--- convertLocalMode.sh
+++ convertLocalMode.sh
@@ -6,3 +6,9 @@ if [ -z "$GIT_ROOT" ]; then
     echo "Set a git repository first"
     exit 1
 fi
+
+while IFS= read -r -d '' repo_source
+do
+    echo $repo_source
+
+done <   <(find "$GIT_ROOT" -iname "*.git" -print0)
