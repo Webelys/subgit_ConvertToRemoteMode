00c56b51f7b1a895ba57d24cd8279b3f371024ad
cam.lafit <cam.lafit@azerttyu.net>
2017-03-06 16:54:27 +0100

    Correct some double quote
    * We muse expand some data
    * https://github.com/koalaman/shellcheck/wiki/SC2086

--- convertLocalMode.sh
+++ convertLocalMode.sh
@@ -60,20 +60,20 @@ do
 
         ## Manage svn data
         rm -fr "$repo_convert/svn/refs/svn/root"
-        cp -far "$repo_source/svn/refs/svn/root/$svn_path" "$repo_convert/svn/refs/svn/root"
-        cp -far "$repo_source/svn/.metadata" "$repo_convert/svn/"
+        cp -far "$repo_source"/svn/refs/svn/root/"$svn_path" "$repo_convert"/svn/refs/svn/root
+        cp -far "$repo_source"/svn/.metadata "$repo_convert"/svn/
 
         ## Manage refs
-        cp -far "$repo_source/refs" "$repo_convert/"
-        rm -fr "$repo_convert/refs/svn/{root,attic}"
-        cp -far "$repo_source/refs/svn/root/$svn_path" "$repo_convert/refs/svn/root"
-        cp -far "$repo_source/refs/svn/attic/$svn_path" "$repo_convert/refs/svn/attic"
+        cp -far "$repo_source"/refs "$repo_convert"/
+        rm -fr "$repo_convert"/refs/svn/{root,attic}
+        cp -far "$repo_source"/refs/svn/root/"$svn_path" "$repo_convert"/refs/svn/root
+        cp -far "$repo_source"/refs/svn/attic/"$svn_path" "$repo_convert"/refs/svn/attic
 
         git --git-dir="$repo_convert" fetch --force "$repo_source" refs/svn/map:refs/svn/map
 
         ## Get packed refs (if existing)
-        rm "$repo_convert/packed-refs"
-        cp -bar "$repo_source/packed-refs" "$repo_convert/packed-refs"
+        rm "$repo_convert"/packed-refs
+        cp -bar "$repo_source"/packed-refs "$repo_convert"/packed-refs
 
         ##Remove remotes
         git --git-dir="$repo_convert" branch -rd "$(git --git-dir=$repo_convert branch -r)"
