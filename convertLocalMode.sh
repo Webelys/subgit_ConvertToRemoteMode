732cb9f1026e2248312995f51665252da1a08576
cam.lafit <cam.lafit@azerttyu.net>
2017-03-06 11:28:11 +0100

    Define project git source and target

--- convertLocalMode.sh
+++ convertLocalMode.sh
@@ -10,5 +10,12 @@ fi
 while IFS= read -r -d '' repo_source
 do
     echo $repo_source
+    repo_name=$(basename $repo_source .git)
+    repo_convert=$CONVERT_REPO/$repo_name.git
+
+    if [ ! -d "$repo_source" ]; then
+            echo "No repo source set"
+            exit 1;
+    fi
 
 done <   <(find "$GIT_ROOT" -iname "*.git" -print0)
